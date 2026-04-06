2ed5791cf240c06d87a423d8b7ad9ae8fb4ffb2b
Raja Kumar <kraj9380286@gmail.com>
2026-04-06 16:57:28 +0530

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/is-anagram/submission-0.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size()!=t.size())return false;
+        unordered_map<int,int>mp;
+        for(int i=0;i<s.size();i++){
+            mp[s[i]]++;
+        }
+        for(int i=0;i<t.size();i++){
+            mp[t[i]]--;
+        }
+        for(auto val:mp){
+            if(val.second>0)return false;
+        }
+        return true;
+    }
+};
